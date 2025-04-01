b8898d76faf9770e5ffae6e28a03d86de7e2a90f
Cheng Hsuan Lin <chlin2025@gmail.com>
2025-03-31 18:58:02 -0700

    initial setup

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { createServer } from "node:http";
+
+const port = process.env.PORT || 3000;
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ status: "Hello from backend" }));
+});
+
+server.listen(port, () => {
+  console.log(`Server start running...`);
+});
