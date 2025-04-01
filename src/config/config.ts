7a49c6a450885ca9a8fa9d0e0124adfe9be73eca
Cheng Hsuan Lin <chlin2025@gmail.com>
2025-04-01 01:19:00 -0700

    check if api key exists

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+if (!process.env.API_KEY) {
+  throw new Error("Missing API_KEY");
+}
+
 export const config = {
   apiKey: process.env.API_KEY,
   baseUrl: "https://api.openweathermap.org/data/2.5/",
