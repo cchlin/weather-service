05ddd2183e6fded0d1c4506f3f0d6079df918a11
Cheng Hsuan Lin <chlin2025@gmail.com>
2025-04-02 21:55:28 -0700

    return raw json instead of parsed object so it focus on interact with the api

--- src/service/WeatherApi.ts
+++ src/service/WeatherApi.ts
@@ -5,40 +5,12 @@ import { mapToFields } from "../util/transformers";
 const UNIT = "imperial";
 
 export class WeatherApi {
-  async getCurrentWeather(city: string): Promise<CurrentWeather | undefined> {
-    const data = await this.fetchData("weather", city);
-
-    if (!data) return;
-
-    // console.log(`Current weather in ${city}: `, data);
-
-    const weatherData = mapToFields(data);
-    return {
-      city: data.name,
-      timestamp: new Date(data.dt * 1000), // response is in second but js/ts Date needs milliseconds so * 1000
-      ...weatherData,
-    };
+  async getCurrentWeather(city: string): Promise<any | undefined> {
+    return await this.fetchData("weather", city);
   }
 
-  async getForecast(city: string): Promise<Forecast[] | undefined> {
-    const data = await this.fetchData("forecast", city);
-
-    if (!data) return;
-
-    // console.log(`Forecast for ${city}: `, data);
-
-    const fetchedTime = new Date();
-
-    return data.list.map((entry: any) => {
-      const weatherData = mapToFields(entry);
-
-      return {
-        city: data.city.name,
-        forecast_time: new Date(entry.dt * 1000),
-        fetched_time: fetchedTime,
-        ...weatherData,
-      };
-    });
+  async getForecast(city: string): Promise<any | undefined> {
+    return await this.fetchData("forecast", city);
   }
 
   private async fetchData(endpoint: string, city: string): Promise<any> {
