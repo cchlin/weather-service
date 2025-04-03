4d2d667b90dcbcfb264d8ee70067aa782a7b04a0
Cheng Hsuan Lin <chlin2025@gmail.com>
2025-04-03 00:38:40 -0700

    working in progress but i need to change a machine

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,33 @@ import { WeatherApi } from "./service/WeatherApi";
 import { WeatherRepo } from "./repository/WeatherRepo";
 
 const api = new WeatherApi();
+const repo = new WeatherRepo();
 
 const cities: string[] = ["Seattle", "New York", "Paris", "Taipei", "Tokyo"];
 
 const run = async () => {
   // for (const city of cities) {
-  console.log(await api.getCurrentWeather("Seattle"));
+  // console.log(await api.getCurrentWeather("Seattle"));
+  const current = await api.getCurrentWeather("Seattle");
+  if (current) {
+    repo.saveCurrentWeather(current);
+  }
   // }
-  console.log(await api.getForecast("Seattle"));
+  // console.log(await api.getForecast("Seattle"));
+  const forecastList = await api.getForecast("Seattle");
+  if (forecastList) {
+    for (const forecast of forecastList) {
+      await repo.saveForecast(forecast);
+    }
+  }
+
+  const list = await repo.getForecast("Seattle");
+  for (const item of list) {
+    console.log(item);
+  }
+  console.log("Forecast mock db retrieval done");
+  console.log(repo.getMostRecentWeather("Seattle"));
+  console.log("Weather mock db retrieval done");
 };
 
 run();
