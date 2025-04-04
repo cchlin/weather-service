25d83e310c5b9d05945e3495c0564fa0e1afb6a7
Cheng Hsuan Lin <chlin2025@gmail.com>
2025-04-03 19:34:09 -0700

    added postgres package and configured db connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,11 @@
 import dotenv from "dotenv";
+import postgres from "postgres";
 
 dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL");
+}
 
 if (!process.env.API_KEY) {
   throw new Error("Missing API_KEY");
@@ -9,4 +14,5 @@ if (!process.env.API_KEY) {
 export const config = {
   apiKey: process.env.API_KEY,
   baseUrl: "https://api.openweathermap.org/data/2.5/",
+  sql: postgres(process.env.DATABASE_URL),
 };
